772a11a90b03bff82e0e6550b469836c18724d71
Rodrigo Magalhães <rodrigorm.rm@gmail.com>
2019-09-18 15:58:38 -0300

    Implementação

--- src/main/java/br/com/test/Teste.java
+++ src/main/java/br/com/test/Teste.java
@@ -1,18 +1,26 @@
+package br.com.test;
+
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class Teste_ {
+public class Teste {
 
-    public static void main(String[] args) {
+    @Test
+    public void test_() {
         Response response =  RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
 
-        System.out.println(response.getBody().asString().equals("Ola Mundo!"));
-        System.out.println(response.getStatusCode() == 200);
+        Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+        Assert.assertTrue((response.getStatusCode() == 200));
+        Assert.assertEquals("201",response.getStatusCode());
 
         ValidatableResponse validatableResponse = response.then();
         validatableResponse.statusCode(201);
-
     }
+
+
+
 }
