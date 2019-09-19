cf3d915dea727c452508907bd60a873f55e53908
Rodrigo Magalhães <rodrigorm.rm@gmail.com>
2019-09-19 14:50:19 -0300

    implementação

--- src/main/java/br/com/test/Teste.java
+++ src/main/java/br/com/test/Teste.java
@@ -4,8 +4,16 @@ import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertThat;
 
 public class Teste {
 
@@ -21,6 +29,42 @@ public class Teste {
         validatableResponse.statusCode(201);
     }
 
+    @Test
+    public void teste2() {
+        get("http://restapi.wcaquino.me/ola").then().statusCode(202);
 
+    }
+
+    @Test
+    public void teste_3() {
+        given()
+        .when()
+            .get("http://restapi.wcaquino.me/ola")
+        .then()
+                .statusCode(202);
+    }
+
+    @Test
+    public void testHamcrest() {
+        assertThat("Rodrigo", Matchers.is("Rodrigo"));
+        assertThat(123, Matchers.isA(Integer.class));
+        assertThat(123, Matchers.greaterThan(22));
+
+        List<Integer> odd = Arrays.asList(1, 3, 5, 7, 9);
+        assertThat(odd, Matchers.hasSize(5));
+        assertThat(odd, Matchers.hasItem(5));
+
+    }
+
+    @Test
+    public void shouldValidateBody() {
+        given()
+                .when()
+                .get("http://restapi.wcaquino.me/ola")
+                .then()
+                .statusCode(200)
+                .body(Matchers.is("Ola Mundo!"));
+
+    }
 
 }
