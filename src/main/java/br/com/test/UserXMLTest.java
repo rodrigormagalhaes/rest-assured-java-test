6d6f567b2bfb2010f6d87e8a80ae34d2c7c1ec93
Rodrigo Magalhães <tqi_rmagalhaes@uolinc.com>
2019-09-24 09:59:56 -0300

    impl

--- src/main/java/br/com/test/UserXMLTest.java
+++ src/main/java/br/com/test/UserXMLTest.java
@@ -13,13 +13,20 @@ public class UserXMLTest {
                     .get("http://restapi.wcaquino.me/usersXML/3")
                 .then()
                     .statusCode(200)
-                    .body("user.name", Matchers.is("Ana Julia"))
-                    .body("user.@id", Matchers.is("3"))
-                    .body("user.filhos.name.size()", Matchers.is(2))
-                    .body("user.filhos.name[0]", Matchers.is("Zezinho"))
-                    .body("user.filhos.name[1]", Matchers.is("Luizinho"))
-                    .body("user.filhos.name", Matchers.hasItem("Luizinho"))
-                    .body("user.filhos.name", Matchers.hasItems("Luizinho", "Luizinho"))
+
+                    .rootPath("user")
+                    .body("name", Matchers.is("Ana Julia"))
+                    .body("@id", Matchers.is("3"))
+                    .rootPath("user.filhos")
+                    .body("name.size()", Matchers.is(2))
+
+                    .detachRootPath("filhos")
+                    .body("filhos.name[0]", Matchers.is("Zezinho"))
+                    .body("filhos.name[1]", Matchers.is("Luizinho"))
+
+                    .appendRootPath("filhos")
+                    .body("name", Matchers.hasItem("Luizinho"))
+                    .body("name", Matchers.hasItems("Luizinho", "Luizinho"))
         ;
     }
 }
