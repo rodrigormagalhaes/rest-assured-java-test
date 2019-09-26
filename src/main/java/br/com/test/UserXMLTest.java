04c1288e8e0767001005380b17a3f48fe0bdb782
Rodrigo Magalhães <rodrigorm.rm@gmail.com>
2019-09-26 07:56:47 -0300

    implementação

--- src/main/java/br/com/test/UserXMLTest.java
+++ src/main/java/br/com/test/UserXMLTest.java
@@ -29,4 +29,23 @@ public class UserXMLTest {
                     .body("name", Matchers.hasItems("Luizinho", "Luizinho"))
         ;
     }
+
+    @Test
+    public void shouldDoAdvancedSearchWithXML() {
+        RestAssured.given()
+                .when()
+                    .get("http://restapi.wcaquino.me/usersXML")
+                .then()
+                    .statusCode(200)
+                    .body("users.user.size()", Matchers.is(3))
+                    .body("users.user.findAll{it.age.toInteger() <= 25}.size()", Matchers.is(2))
+                    .body("users.user.@id", Matchers.hasItems("1", "2", "3"))
+                    .body("users.user.find{it.age == 25}.name", Matchers.is("Maria Joaquina"))
+                    .body("users.user.findAll{it.name.toString().contains('n')}.name", Matchers.hasItems("Maria Joaquina", "Ana Julia"))
+                    .body("users.user.salary.find{it != null}.toDouble()", Matchers.is(1234.5678d))
+                    .body("users.user.age.collect{it.toInteger() * 2}", Matchers.hasItems(40, 50, 60))
+                    .body("users.user.name.findAll{it.toString().startsWith('Maria')}.collect{it.toString().toUpperCase()}", Matchers.is("MARIA JOAQUINA"))
+
+        ;
+    }
 }
